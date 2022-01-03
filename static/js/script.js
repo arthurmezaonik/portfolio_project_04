9b20d8ba04f816249cc67b0796cd8e728ff818fe
Arthur Martins <arthur.mezaonik@gmail.com>
2022-01-03 13:09:30 +0000

    Finish filter function

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,76 @@
-function hideAll(){
-    let publications = document.getElementsByClassName('publications');
-    for (publication of publications){
-        publication.classList.toggle('hide');
-    }   
-}
-
-function showSelected(value){
-    let selectedPublications = document.querySelectorAll('[data-type="'+value+'"]')
-    for (publication of selectedPublications){
-        publication.classList.toggle('hide'); 
+// Add hide class to all publications
+function addHideClass(){
+    let allPublications = document.getElementsByClassName('publications');
+    for (publication of allPublications){
+        publication.classList.add('hide');
     }
 }
 
+// Remove hide class from all publication
+function removeHideClass(){
+    let allPublications = document.getElementsByClassName('publications');
+    for (publication of allPublications){
+        if(publication.classList.contains('hide')){
+            publication.classList.remove('hide');
+        }
+    }
+}
+
+// Remove hide class only for those who match the filter
+function showFilteredPublications(value){
+    let filteredPublications = document.querySelectorAll('[data-type="'+value+'"]');
+    for (publication of filteredPublications){
+        publication.classList.remove('hide');
+    }
+}
+
+// Remove checked from all inputs
+function clearInputs(){
+    let allInputs = document.querySelectorAll('input[name="job_types"]');
+    for (input of allInputs){
+        input.checked=false;
+    }
+}
+
+// Add checked back to the chosen input
+function checkInput(value){
+    let input = document.querySelector('input[value="'+value+'"]');
+    input.checked=true;
+}
+
+// Add opacity class to not selected inputs labels
+function addOpacityClass(){
+    let allInputs = document.querySelectorAll('input[name="job_types"');
+    for (input of allInputs){
+        if(input.checked == false){
+            input.previousElementSibling.classList.add('opacity')
+        }
+    }
+}
+
+// Remove opacity class from all input labels
+function removeOpacityClass(){
+    let allLabels = document.getElementsByTagName('label');
+    for(label of allLabels){
+        if(label.classList.contains('opacity')){
+            label.classList.remove('opacity');
+        }
+    }
+}
+
+// Main filter function
 function jobFilter(value){
-    hideAll()
-    showSelected(value)
+    let firstTimeClicked = document.querySelector('input[value="'+value+'"]').checked;
+    
+    if(firstTimeClicked){
+        clearInputs();
+        checkInput(value);
+        addHideClass();
+        removeOpacityClass()
+        addOpacityClass();
+        showFilteredPublications(value);
+    }else{
+        removeHideClass();
+        removeOpacityClass();
+    }
 }
