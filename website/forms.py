dc59000c18ae715fdfc05de8cdfe8a87299ab2a9
Arthur Martins <arthur.mezaonik@gmail.com>
2022-01-05 23:56:25 +0000

    Add reasons on index page

--- website/forms.py
+++ website/forms.py
@@ -3,9 +3,18 @@ from django.forms import ModelForm, widgets
 from .models import Publication
 
 class PublicationForm(ModelForm):
+    """Form to create a new publication"""
     class Meta:
         model = Publication
-        fields = ('title', 'author_fname', 'author_lname', 'small_description', 'description', 'contact_email', 'job_type',)
+        fields = (
+            'title',
+            'author_fname',
+            'author_lname',
+            'small_description',
+            'description',
+            'contact_email',
+            'job_type',
+            )
 
         labels = {
             'title' : '',
@@ -18,9 +27,19 @@ class PublicationForm(ModelForm):
 
         widgets = {
             'title' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Title*',}),
-            'author_fname' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name*',}),
-            'author_lname' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name*',}),
-            'small_description' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Small Description',}),
-            'description' : forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description',}),
-            'contact_email' : forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Contact Email*',}),
+            'author_fname' : forms.TextInput(attrs={
+                'class':'form-control', 'placeholder':'First Name*',
+            }),
+            'author_lname' : forms.TextInput(attrs={
+                'class':'form-control', 'placeholder':'Last Name*',
+            }),
+            'small_description' : forms.TextInput(attrs={
+                'class':'form-control', 'placeholder':'Small Description',
+            }),
+            'description' : forms.Textarea(attrs={
+                'class':'form-control', 'placeholder':'Description',
+            }),
+            'contact_email' : forms.EmailInput(attrs={
+                'class':'form-control', 'placeholder':'Contact Email*',
+            }),
         }
